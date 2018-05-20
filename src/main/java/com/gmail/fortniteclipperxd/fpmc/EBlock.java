ffa22989cc95e9b1d95d4e27a1e229523ff44c13
FloM77 <fortniteclipperxd@gmail.com>
2018-05-21 00:14:56 +0200

    Owner in Blockstate

--- src/main/java/com/gmail/fortniteclipperxd/fpmc/EBlock.java
+++ src/main/java/com/gmail/fortniteclipperxd/fpmc/EBlock.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemFlag;
@@ -11,12 +12,15 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.bukkit.Bukkit.getLogger;
 import static org.bukkit.Bukkit.getPluginManager;
+import static org.bukkit.Bukkit.getServer;
 
 public class EBlock extends EBase {
-    public ArrayList<Location> Logical = new ArrayList<Location>();
+    public HashMap<Location, String> Logical;
     public static ArrayList<EBlock> All = new ArrayList<EBlock>();
     public static P1 config;
 
@@ -25,12 +29,12 @@ public class EBlock extends EBase {
         super(name, appearance);
         All.add(this);
 
-        LoadState();
+        Logical = (LoadState() != null ? LoadState() : new HashMap<Location, String>());
     }
 
     public void PlacedEvent(BlockPlaceEvent e)
     {
-        Logical.add(e.getBlockPlaced().getLocation());
+        Logical.put(e.getBlockPlaced().getLocation(), e.getPlayer().getPlayerListName());
 
         SaveState();
     }
@@ -47,8 +51,8 @@ public class EBlock extends EBase {
     {
         for(EBlock eb : All)
         {
-            for(Location l: eb.Logical) {
-                if (l.equals(location)) {
+            for(Map.Entry<Location, String> l: eb.Logical.entrySet()) {
+                if (l.getKey().equals(location)) {
                     return eb;
                 }
             }
@@ -56,15 +60,56 @@ public class EBlock extends EBase {
         return null;
     }
 
+    public static HashMap<String, Location> SearchAllByPlayer(String playername)
+    {
+        HashMap<String, Location> ret = new HashMap<String, Location>();
+        for(EBlock eb : All)
+        {
+            for(Map.Entry<Location, String> l: eb.Logical.entrySet()) {
+                if (l.getValue().equals(playername)) {
+                    ret.put(eb.Name, l.getKey());
+                }
+            }
+        }
+        return ret.size() == 0 ? null : ret;
+    }
+
+    public ArrayList<Location> SearchByPlayer(String playername)
+    {
+        ArrayList<Location> ret = new ArrayList<Location>();
+        for(Map.Entry<Location, String> l: Logical.entrySet()) {
+            if (l.getValue().equals(playername)) {
+                ret.add(l.getKey());
+            }
+        }
+        return ret.size() == 0 ? null : ret;
+    }
+
     public void SaveState()
     {
-        config.getConfig().set("World.EBlockState." + Name, Logical);
+        ArrayList<Location> listLocations = new ArrayList<Location>();
+        ArrayList<String> listPlayers = new ArrayList<String>();
+        for(Map.Entry<Location, String> l : Logical.entrySet())
+        {
+            listLocations.add(l.getKey());
+            listPlayers.add(l.getValue());
+        }
+        config.getConfig().set("World.EBlockState." + Name + ".Locations", listLocations);
+        config.getConfig().set("World.EBlockState." + Name + ".Players", listPlayers);
         config.saveConfig();
     }
 
-    public void LoadState()
+    public HashMap<Location, String> LoadState()
     {
-        if(config.getConfig().get("World.EBlockState." + Name) != null)
-        Logical = (ArrayList<Location>) config.getConfig().get("World.EBlockState." + Name);
+        if(config.getConfig().get("World.EBlockState." + Name + ".Locations") == null ||
+            config.getConfig().get("World.EBlockState." + Name + ".Players") == null) return null;
+        HashMap load = new HashMap<Location, String>();
+        ArrayList<Location> locations = (ArrayList<Location>) config.getConfig().get("World.EBlockState." + Name + ".Locations");
+        ArrayList<String> players = (ArrayList<String>) config.getConfig().get("World.EBlockState." + Name + ".Players");
+        for(int i=0;i<locations.size();i++)
+        {
+            load.put(locations.get(i), players.get(i));
+        }
+        return load;
     }
 }
